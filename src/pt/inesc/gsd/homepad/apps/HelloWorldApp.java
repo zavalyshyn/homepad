8eddade92b16f43e6afb8936325ebbe3175a151e
heroinsoul <i.zavalishin@gmail.com>
2017-10-30 16:35:55 +0000

    Added HelloWorld app

---
+++ src/pt/inesc/gsd/homepad/apps/HelloWorldApp.java
@@ -0,0 +1,37 @@
+package pt.inesc.gsd.homepad.apps;
+
+import pt.inesc.gsd.homepad.AppElements;
+import pt.inesc.gsd.homepad.Logger;
+import pt.inesc.gsd.homepad.annotations.BodyAppLayoutElement;
+import pt.inesc.gsd.homepad.modules.ports.DoorLockStatusPortType;
+import pt.inesc.gsd.homepad.modules.ports.NullPortType;
+import pt.inesc.gsd.homepad.server.runtime.AppLayoutElement;
+import pt.inesc.gsd.homepad.server.runtime.LayoutEvent;
+
+public class HelloWorldApp extends AppElements{
+	
+	private static final Logger LOGGER = Logger.getLogger(HelloWorldApp.class);
+	
+	@BodyAppLayoutElement(name = "HelloWorldBegin")
+	public void handleHelloWorldBegin(AppLayoutElement el, LayoutEvent event) {
+		LOGGER.log(Logger.AP, "Invoked: HelloWorldBegin.");
+		el.sendEventToPort(new NullPortType(), 0);
+	}
+	
+	@BodyAppLayoutElement(name = "HelloWorldCheck")
+	public void handleHelloWorldCheck(AppLayoutElement el, LayoutEvent event) {
+		LOGGER.log(Logger.AP, "Invoked: HelloWorldCheck.");
+		if (event instanceof DoorLockStatusPortType) {
+			DoorLockStatusPortType e = (DoorLockStatusPortType) event;
+			if (e.getDoorLockStatus() == DoorLockStatusPortType.DOOR_LOCK_LOCKED) {
+				el.sendEventToPort(new NullPortType(), 0);
+			}
+		}
+	}
+
+	@BodyAppLayoutElement(name = "HelloWorldEnd")
+	public void handleHelloWorldEnd(AppLayoutElement el, LayoutEvent event) {
+		LOGGER.log(Logger.AP, "Invoked: HelloWorldEnd.");
+	}
+
+}
---
+++ src/pt/inesc/gsd/homepad/apps/HelloWorldAppManifest.java
@@ -0,0 +1,57 @@
+package pt.inesc.gsd.homepad.apps;
+
+import pt.inesc.gsd.homepad.AppManifest;
+import pt.inesc.gsd.homepad.Layout;
+
+public class HelloWorldAppManifest extends AppManifest{
+	
+	@Override
+	public String getAppName() {
+		return "HelloWorldApp";
+	}
+
+	@Override
+	public String getAppElementClassName() {
+		return "pt.inesc.gsd.homepad.apps.HelloWorldApp";
+	}
+
+	@Override
+	public String[] getRequiredElements() {
+		return new String[] {
+			"FromCamera",
+			"CloudCall"
+		};
+	}
+
+	@Override
+	public String[] getDeclaredElements() {
+		return new String[] {
+			"HelloWorldBegin",
+			"HelloWorldCheck",
+			"HelloWorldEnd"
+		};
+	}
+
+	@Override
+	public Layout getLayout() {
+		Layout layout = new Layout();
+		
+		// specify the nodes
+		String[] n = {
+			"FromCamera",		// 1
+			"HelloWorldBegin",	// 2
+			"HelloWorldCheck",	// 3
+			"CloudCall",		// ...
+			"HelloWorldEnd"
+		};
+		for (String s : n) layout.addNode(s);
+
+		// specify the edges
+		layout.addEdge(1, 2);
+		layout.addEdge(2, 3);
+		layout.addEdge(3, 4); // port: 0
+		layout.addEdge(4, 5);
+		return layout;
+	}
+
+}
