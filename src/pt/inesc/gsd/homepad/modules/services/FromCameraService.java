b9ddc6326aaaead28ad7b7a2efe2b21336097f3b
zavalyshyn <i.zavalishin@gmail.com>
2017-11-03 10:25:51 +0000

    updated FromCamera service to grab frames from actual IP camera

--- src/pt/inesc/gsd/homepad/modules/services/FromCameraService.java
+++ src/pt/inesc/gsd/homepad/modules/services/FromCameraService.java
@@ -1,5 +1,15 @@
 package pt.inesc.gsd.homepad.modules.services;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
 import pt.inesc.gsd.homepad.modules.messages.FromCameraGlobalEvent;
 import pt.inesc.gsd.homepad.server.Component;
 import pt.inesc.gsd.homepad.server.GlobalEvent;
@@ -8,6 +18,15 @@ public class FromCameraService extends Component {
 
 	private Thread _cameraPooler = null;
 	private boolean _cameraPoolerRunning = false;
+	
+	private InputStream urlStream;
+	private StringWriter stringWriter;
+	private boolean processing = true;
+	
+//	private static final String STREAM_URL = "http://96.10.1.168/mjpg/1/video.mjpg";
+//	private static final String STREAM_URL = "http://wmccpinetop.axiscam.net/mjpg/video.mjpg";
+	private static final String STREAM_URL = "http://70.185.95.146/mjpg/video.mjpg";
+	private static final String CONTENT_LENGTH = "Content-Length: ";
 
 	@Override
 	public boolean canHandleEventType(Class<? extends GlobalEvent> eventType) {
@@ -21,17 +40,98 @@ public class FromCameraService extends Component {
 		    public void run() {
 	        	while(_cameraPoolerRunning) {
 			        try {
-			        	Thread.sleep(1000);
-			        	sendOutgoingEvent(new FromCameraGlobalEvent(new byte[100]));
+
+			        	
+			        	// Open the stream
+						URL url = new URL(STREAM_URL);
+						URLConnection urlConn = url.openConnection();
+						// change the timeout to taste, I like 1 second
+						urlConn.setReadTimeout(1000);
+						urlConn.connect();
+						urlStream = urlConn.getInputStream();
+						stringWriter = new StringWriter(128);
+						
+						// crop the frame header
+						boolean haveHeader = false; 
+						int currByte = -1;
+						String header = "";
+						
+						while((currByte = urlStream.read()) > -1 && !haveHeader) {
+							stringWriter.write(currByte);
+							
+							String tempString = stringWriter.toString(); 
+							
+							// Count how many lines tempString contains
+							String findStr = "\n";
+							int lastIndex = 0;
+							int count = 0;
+							while(lastIndex != -1){
+							    lastIndex = tempString.indexOf(findStr,lastIndex);
+							    if(lastIndex != -1){
+							        count ++;
+							        lastIndex += findStr.length();
+							    }
+							}
+							
+							header = tempString;
+							if (count==3) haveHeader = true; // each header consists of 3 lines before the frame bytes begin
+						}
+						
+						
+//						System.out.println(header);
+						
+						// 255 indicates the start of the jpeg image
+						while((urlStream.read()) != 255)
+						{
+							// just skip extras
+						}
+						
+						// process the frame bytes
+						int contentLength = contentLength(header);
+						byte[] imageBytes = new byte[contentLength + 1];
+						// since we ate the original 255 , shove it back in
+						imageBytes[0] = (byte)255;
+						int offset = 1;
+						int numRead = 0;
+						while (offset < imageBytes.length
+							&& (numRead=urlStream.read(imageBytes, offset, imageBytes.length-offset)) >= 0) 
+						{
+							offset += numRead;
+						}   
+						
+						stringWriter = new StringWriter(128);
+
+			        	
+		        	 Thread.sleep(1000);
+		        	 sendOutgoingEvent(new FromCameraGlobalEvent(imageBytes));	
+			        	
 			        } catch(InterruptedException v) {
 			        	return;
-			        }
+			        } catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 	        	}
 		    }
 		};
 		_cameraPooler.start();
 	}
 
+	
+	// dirty but it works content-length parsing
+	private static int contentLength(String header)
+	{
+		int indexOfContentLength = header.indexOf(CONTENT_LENGTH);
+		int valueStartPos = indexOfContentLength + CONTENT_LENGTH.length();
+		int indexOfEOL = header.indexOf('\n', indexOfContentLength);
+		
+		String lengthValStr = header.substring(valueStartPos, indexOfEOL).trim();
+		
+		int retValue = Integer.parseInt(lengthValStr);
+		
+		return retValue;
+	}
+	
 	@Override
 	protected void handleIncomingEvent(GlobalEvent event) {
 	}
